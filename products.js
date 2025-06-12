0ee97bc83f1b6765ead7b5a9d69a37fb379f5ca0
sarasskincare <sarashahpdx@gmail.com>
2025-06-12 14:25:26 -0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    name: "CeraVe Foaming Facial Cleanser",
+    brand: "CeraVe",
+    skinTypes: ["oily", "combination"],
+    concerns: ["acne", "clogged pores"],
+    ingredients: ["AQUA / WATER / EAU, COCAMIDOPROPYL HYDROXYSULTAINE, GLYCERIN, SODIUM LAUROYL SARCOSINATE, PEG-150 PENTAERYTHRITYL TETRASTEARATE, NIACINAMIDE, PEG-6 CAPRYLIC/CAPRIC GLYCERIDES, SODIUM METHYL COCOYL TAURATE, PROPYLENE GLYCOL, CERAMIDE NP, CERAMIDE AP, CERAMIDE EOP, CARBOMER, METHYLPARABEN, SODIUM CHLORIDE, SODIUM LAUROYL LACTYLATE, CHOLESTEROL, DISODIUM EDTA, PROPYLPARABEN, CITRIC ACID, TETRASODIUM EDTA, HYDROLYZED HYALURONIC ACID, PHYTOSPHINGOSINE, XANTHAN GUM.
+"],
+    allergies: [],
+    pH: 5.5,
+    fragrenceType:
+    description: "A gentle foaming cleanser ideal for removing excess oil without stripping the skin. Fragrance-free and non-comedogenic.",
+    link: "https://www.cerave.com/our-products/cleansers/foaming-facial-cleanser",
+    image: "images/cerave-cleanser.jpg"
+  }
+];
